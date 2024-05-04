10aae3eeadd0d0eacb72f28b054f877ef83c6834
fengzhiqiu <fengzhiqiu@users.noreply.github.com>
2024-05-04 11:31:50 +0800

    修改404图片

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -383,7 +383,7 @@ const Layout404 = props => {
               {/* 左侧动图 */}
               <LazyImage
                 className="error-img h-60 md:h-full p-4"
-                src={'https://bu.dusays.com/2023/03/03/6401a7906aa4a.gif'}
+                src={'https://img.techins.xyz/techins/2023/11/404.gif'}
               ></LazyImage>
 
               {/* 右侧文字 */}
