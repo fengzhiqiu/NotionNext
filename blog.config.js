509d1caea3939afc520bb99398636a383040a8db
fengzhiqiu <fengzhiqiu@users.noreply.github.com>
2024-05-07 23:16:34 +0800

    Update domain

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '夏夜', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.techins.xyz', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://techins.xyz', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '科技Ins, techins, 网站建设, 实用工具, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
